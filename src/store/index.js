9ff73ce90421e9386e58cead770f423510e48b78
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-06-28 14:31:18 +0700

    Creating a Redux Store for React

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from "redux";
+
+// Reducer
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+// store
+const store = createStore(counterReducer);
+
+export default store;
